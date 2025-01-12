262e0a620d6ceb54eff069be56b9367ad48da312
C-Hunter77 <knowledgeisking77@gmail.com>
2025-01-12 16:27:40 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,71 @@
+// TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const path = require('path');
+const generateMarkdown = require('./utils/generateMarkdown');
+
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is your project title?',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'What license would you like to use for your project?',
+        choices: ['MIT', 'Apache', 'GPL', 'BSD'],
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please provide a description of your project.',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the installation instructions for your project?',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'What is the usage information for your project?',
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'What are the contribution guidelines for your project?',
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'What are the test instructions for your project?',
+    },
+    {
+        type: 'input',
+        name: 'username',
+        message: 'What is your GitHub username?',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?',
+    },
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+}
+
+// TODO: Create a function to initialize app
+function init() {
+    inquirer.prompt([...questions]).then((answers) => {
+        const markdown = generateMarkdown(answers);
+        writeToFile('README.md', markdown);
+    });
+}
+
+// Function call to initialize app
+init();
